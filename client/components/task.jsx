a03874904cecbecf578ce1e9bd8e19b56eebda5a
ay7991 <amy.yang7991@gmail.com>
2024-01-08 17:17:44 -0500

    added hidden input field for date, adjusted server to just res.status

--- client/components/task.jsx
+++ client/components/task.jsx
@@ -5,49 +5,33 @@ import { addTask } from '../reducers/taskSlice.js';
 
 const Task = () => {
     const { buttonClicked } = useSelector((state) => state.task);
-    //console.log(buttonClicked);
+    console.log(buttonClicked);
     const dispatch = useDispatch();
 
-    const handleClick = async (event) => {
-        console.log(event);
-        event.preventDefault();
-        const rawDate = Date.Now();
-            const day = rawDate.getDate();
-            const month = rawDate.getMonth();
-            const year = rawDate.getYear();
-            const date = `${day}-${month}-${year}`;
-        // sample userTasks endpoint, check in with backend team
+    const rawDate = new Date();
+        const day = rawDate.getDate();
+        const month = rawDate.getMonth();
+        const year = rawDate.getYear();
+        const date = `${day}-${month}-${year}`;
 
-        await fetch('/addTasks', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            // sample task property name, check how the SQL table is labeled
-            body: JSON.stringify({
-                user_id: '01', 
-                date: date, 
-                title: document.getElementById('title').value, 
-                description: document.getElementById('description').value 
-            })
-        })
-        .then((res) => {
-            console.log(res);
-            res.json()})
-        .then((data) => console.log('Data: ' + data))
-        .catch((error) => console.log(error));
-}
-
+    //const date = '2024-01-08';
+    
     return (
         <div>
             <button onClick={() => dispatch(addTask())}> New Task </button>
-            <div>
-                <form method="POST" action="/addTasks"> 
-                    <input type="text" name="title" placeholder="Title" id="title" />
-                    <input type="text" name="description" placeholder="Description" id="description" />
-                    <button type="submit" onSubmit={handleClick}> Submit </button>
-                </form>
-            </div>
+
+            { buttonClicked ?  
+                <div>
+                    <form method="POST" action="/addTasks"> 
+                        <input type="text" name="title" placeholder="Title" id="title" />
+                        <input type="text" name="description" placeholder="Description" id="description" />
+                        <input type="hidden" name="date" value={date} />
+                        <button type="submit"> Submit </button>
+                    </form>
+                </div>
+                :
+                ''
+            }
             
         </div>
     )
@@ -55,3 +39,35 @@ const Task = () => {
 }
 
 export default Task;
+
+// handleClick function
+
+// const handleClick = async (event) => {
+    //     console.log(event);
+    //     event.preventDefault();
+    //     const rawDate = Date.Now();
+    //         const day = rawDate.getDate();
+    //         const month = rawDate.getMonth();
+    //         const year = rawDate.getYear();
+    //         const date = `${day}-${month}-${year}`;
+    //     // sample userTasks endpoint, check in with backend team
+
+    //     await fetch('/addTasks', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         // sample task property name, check how the SQL table is labeled
+    //         body: JSON.stringify({
+    //             user_id: '01', 
+    //             date: date, 
+    //             title: document.getElementById('title').value, 
+    //             description: document.getElementById('description').value 
+    //         })
+    //     })
+    //     .then((res) => {
+    //         console.log(res);
+    //         res.json()})
+    //     .then((data) => console.log('Data: ' + data))
+    //     .catch((error) => console.log(error));
+// }
