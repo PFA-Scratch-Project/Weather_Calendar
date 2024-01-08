5231b07fa81c69d55c16f368b9acdbae215fcb26
ay7991 <amy.yang7991@gmail.com>
2024-01-08 12:28:01 -0500

    added handleclick functionality in taskSlice

--- client/components/task.jsx
+++ client/components/task.jsx
@@ -8,20 +8,6 @@ const Task = () => {
     console.log(newTask);
     const dispatch = useDispatch();
 
-    const handleClick = async (event) => {
-        // sample userTasks endpoint, check in with backend team
-        await fetch('/userTasks', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            // sample task property name, check how the SQL table is labeled
-            body: JSON.stringify({task: document.getElementById('newTask').value})
-        })
-        .then((res) => res.json())
-        .catch((error) => console.log(error));
-    }
-
     return (
         <div>
             <button onClick={() => dispatch(addTask())}> New Task </button>
--- client/reducers/taskSlice.js
+++ client/reducers/taskSlice.js
@@ -8,11 +8,25 @@ const taskSlice = createSlice({
     },
     reducers: {
         addTask: (state, action) => {
+            const handleClick = async (event) => {
+                // sample userTasks endpoint, check in with backend team
+                await fetch('/userTasks', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    // sample task property name, check how the SQL table is labeled
+                    body: JSON.stringify({task: document.getElementById('newTask').value})
+                })
+                .then((res) => res.json())
+                .catch((error) => console.log(error));
+            }
+            
             state.newTask.push(
             <div>
                 <form method="POST" action="/userTasks"> 
                     <input type="text" placeholder="new task" id="newTask" />
-                    <button type="submit"> Submit </button>
+                    <button type="submit" onSubmit={handleClick}> Submit </button>
                 </form>
             </div>);
 
